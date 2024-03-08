a7666ca3a64883fd4d39ca9a4cb164be70c99b7c
surapatsu66 <surapat_su66@live.rmutl.ac.th>
2024-03-08 18:10:34 +0700

    asdas

--- src/components/User.js
+++ src/components/User.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 
-const User = ({ gender, name, location, email, picture, login ,dob}) => {
+const User = ({ gender, name, location, email, picture, login, dob }) => {
     // Function to handle saving user data to the database
     const handleSave = () => {
-        const userData = { gender, name, location, email, picture, login ,dob};
+        const userData = { gender, name, location, email, picture, login, dob };
+        // Convert userData to JSON format
+        const jsonData = JSON.stringify(userData);
         // Assuming you have a function to save user data to the database
-        saveUserData(userData);
+        saveUserData(jsonData);
     };
 
     const saveUserData = (userData) => {
-        axios.post('http://api-user66033.se-rmutl.net/api/saveuser', userData) // ส่งข้อมูลผู้ใช้ไปยังเซิร์ฟเวอร์ Express
+        console.log(userData);
+        axios.post('http://api-user66033.se-rmutl.net/api/saveuser', userData, {
+                headers: {
+                    'Content-Type': 'application/json' // ระบุ Content-Type เป็น JSON
+                }
+            })
             .then(response => {
                 console.log(response.data); // แสดงข้อมูลการตอบกลับจากเซิร์ฟเวอร์
             })
