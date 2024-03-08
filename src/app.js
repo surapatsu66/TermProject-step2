3475d9b9aa60fc6ac3a5920e89ba8c7e73140a3d
surapatsu66 <surapat_su66@live.rmutl.ac.th>
2024-03-08 16:54:37 +0700

    ffff

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import './css/styles.css';
 
 class App extends React.Component {
     componentDidMount() {
-        axios.get('/users')
+        axios.get('https://randomuser.me/api/?page=1&results=10')
             .then(response => {
                 console.log(response.data);
                 store.dispatch(addUsers(response.data.results));
